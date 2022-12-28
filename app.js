d73e4ed89932bb7c1b356e938ca6a7f150089fea
RoHit4100 <rohitudamale777@gmail.com>
2022-12-28 15:17:12 +0530

    mongoose module is install

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const _ = require('lodash');
+const mongoose = require('mongoose');
 const app = express();
 const posts = [];
 const homeContent = "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
@@ -14,6 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
     res.render("home.ejs", { content: homeContent, post: posts })
+
 })
 
 app.get("/about", function (req, res) {
